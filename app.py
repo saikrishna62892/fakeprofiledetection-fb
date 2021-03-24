7f11cc48c760bccd833c9b48a96802acb9da726e
vamsikrishna62892 <vamsikrishna62892@gmail.com>
2021-03-24 08:27:24 +0530

    testing phase deployment 4

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def predict():
     {'fr': 3, 'en': 1, 'nl': 6, 'de': 0, 'tr': 7, 'it': 5, 'gl': 4, 'es': 2}
     '''
     int_features = request.form.values()
+    lang_dict = {'fr': 3, 'en': 1, 'nl': 6, 'de': 0, 'tr': 7, 'it': 5, 'gl': 4, 'es': 2}
     df=pd.DataFrame({'bio':int_features[0],
                      'statuses_count':int_features[1],
                      'followers_count':int_features[5],
@@ -28,25 +29,24 @@ def predict():
                      'favourites_count':int_features[6],
                      'listed_count':int_features[8],
                      'username':int_features[7],
-                     'lang':int_features[3]}, index=[0])
-    lang_dict = {'fr': 3, 'en': 1, 'nl': 6, 'de': 0, 'tr': 7, 'it': 5, 'gl': 4, 'es': 2}
+                     'lang':lang_dict[int_features[3]]}, index=[0])
     sex_predictor = gender.Detector(unknown_value=u"unknown",case_sensitive=False)
     first_name= df['username'].str.split(' ').str.get(0)
     sex= first_name.apply(sex_predictor.get_gender)
     sex_dict={'female': -2, 'mostly_female': -1,'unknown':0,'mostly_male':1, 'male': 2}
     sex_code = sex.map(sex_dict).astype(int)
-    lang_code = int_features[3]
     
- 
+    params = [df['statuses_count'],df['followers_count'],df['friends_count'],df['favourites_count'],df['listed_count'],sex_code,df['lang'].astype(int)]
+    
     #Random forest prediction
-    #rfr_prediction = random_forest.predict([df['statuses_count'],df['followers_count'],df['friends_count'],df['favourites_count'],df['listed_count'],sex_code,lang_dict[lang_code]])
-    rfr_prediction=[0]
+    rfr_prediction = random_forest.predict([df['statuses_count'],df['followers_count'],df['friends_count'],df['favourites_count'],df['listed_count'],sex_code,df['lang']])
+    
     #support vector machine prediction
-    #svm_prediction = support_vector.predict([df['statuses_count'],df['followers_count'],df['friends_count'],df['favourites_count'],df['listed_count'],sex_code,lang_dict[lang_code]])
-    svm_prediction=[0]
+    svm_prediction = support_vector.predict([df['statuses_count'],df['followers_count'],df['friends_count'],df['favourites_count'],df['listed_count'],sex_code,df['lang']])
+    
     #neural network prediction
     ds2 = ClassificationDataSet( 7, 1,nb_classes=2)
-    lst = [df['statuses_count'],df['followers_count'],df['friends_count'],df['favourites_count'],df['listed_count'],sex_code,lang_dict[lang_code]]
+    lst = [df['statuses_count'],df['followers_count'],df['friends_count'],df['favourites_count'],df['listed_count'],sex_code,df['lang']]
     ds2.addSample(lst,1)
     ds2._convertToOneOfMany( )
     fnn_prediction=neural_network.testOnClassData (dataset=ds2)
@@ -60,8 +60,10 @@ def predict():
         percent = 33
     else :
         percent = 0
-    return render_template('result.html', rfr_prediction = rfr_prediction[0],svm_prediction = svm_prediction[0],fnn_prediction = fnn_prediction[0],percentage=percent)
-    #return render_template('index.html')
+    
+    return render_template('result.html', rfr_prediction = rfr_prediction[0],svm_prediction = svm_prediction[0],fnn_prediction = fnn_prediction[0],percentage=percent,features=params)
+
+    #return render_template('index.html',features=params)
 
 
 if __name__ == "__main__":
